f1c0ae3c21da0b5772443cce6fb22048de057c86
TheLastFlame <thelastsight@mail.ru>
2024-03-21 05:08:15 +1000

    add i18n

---
+++ lib/presentation/pages/test.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'package:ya_mafia/zgen/i18n/strings.g.dart';
+
+class TestPage extends StatelessWidget {
+  const TestPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      body: Center(
+        child: Text(t.test),
+      ),
+    );
+  }
+}
