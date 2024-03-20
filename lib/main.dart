65112dec0d7aed749581814f25edc08e9b6d9997
TheLastFlame <thelastsight@mail.ru>
2024-03-20 23:46:31 +1000

    add full screen mode

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,23 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+void settingUpSystemUIOverlay() {
+// Setting SysemUIOverlay
+  SystemChrome.setSystemUIOverlayStyle(
+    const SystemUiOverlayStyle(
+      systemStatusBarContrastEnforced: false,
+      systemNavigationBarColor: Colors.transparent,
+      systemNavigationBarDividerColor: Colors.transparent,
+    ),
+  );
+// Setting SystmeUIMode
+  SystemChrome.setEnabledSystemUIMode(SystemUiMode.edgeToEdge);
+}
 
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  settingUpSystemUIOverlay();
+
   runApp(const MainApp());
 }
 
