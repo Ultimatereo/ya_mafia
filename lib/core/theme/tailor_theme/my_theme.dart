3b9d79b201b9a2128bce8e2800ba0f64abe0567a
TheLastFlame <thelastsight@mail.ru>
2024-03-22 21:50:29 +1000

    add web icon

--- lib/core/theme/tailor_theme/my_theme.dart
+++ lib/core/theme/tailor_theme/my_theme.dart
@@ -5,10 +5,8 @@ part 'my_theme.tailor.dart';
 
 @TailorMixin()
 class MyTheme extends ThemeExtension<MyTheme> with _$MyThemeTailorMixin {
-  /// You can use required / named / optional parameters in the constructor
-  // const MyTheme(this.background);
-  // const MyTheme([this.background = Colors.blue])
   const MyTheme({required this.background});
 
+  @override
   final Color background;
 }
