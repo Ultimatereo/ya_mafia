2a88e0167b944f0d6e6b31b4c50c40aa5f988c86
Ultimatereo <mansursult9@gmail.com>
2024-03-23 17:20:12 +0300

    Added tests for chat gpt

--- lib/domain/gpt/chat_gpt.dart
+++ lib/domain/gpt/chat_gpt.dart
@@ -4,7 +4,9 @@ import 'package:dio/dio.dart';
 import 'package:pretty_dio_logger/pretty_dio_logger.dart';
 import 'package:ya_mafia/domain/gpt/chat_gpt_exception.dart';
 
+/// A class representing an interface to interact with the ChatGPT API.
 class ChatGPT {
+  /// Default options for making HTTP requests.
   static final BaseOptions options = BaseOptions(
     baseUrl: "https://ask.chadgpt.ru/api/public",
     headers: {
@@ -16,21 +18,43 @@ class ChatGPT {
     connectTimeout: const Duration(seconds: 5),
     receiveTimeout: const Duration(seconds: 3),
   );
+
+  /// API key required for authentication with the ChatGPT API.
   final String apiKey;
-  final List<Map<String, String>> history = [];
+
+  /// History of interactions with the ChatGPT assistant.
+  List<Map<String, String>> history = [];
+
+  /// Dio instance for making HTTP requests.
   late final Dio dio;
+
+  /// Function to print log messages.
   final void Function(Object object) logPrint;
 
-  ChatGPT({required this.apiKey, this.logPrint = print}) {
+  /// Constructor for ChatGPT class.
+  ///
+  /// [apiKey]: API key required for authentication. Defaults to environment variable CHAD_GPT_API_KEY.
+  /// [logPrint]: Function to print log messages. Defaults to `print`.
+  ChatGPT({
+    this.apiKey = const String.fromEnvironment("CHAD_GPT_API_KEY"),
+    this.logPrint = print,
+  }) {
     dio = Dio(options);
     dio.interceptors.add(InterceptorsWrapper(
-        onRequest: (RequestOptions options, RequestInterceptorHandler handler) {
-      options.data["api_key"] = apiKey;
-      return handler.next(options);
-    }));
+      onRequest: (RequestOptions options, RequestInterceptorHandler handler) {
+        options.data["api_key"] = apiKey;
+        return handler.next(options);
+      },
+    ));
     dio.interceptors.add(PrettyDioLogger(logPrint: logPrint));
   }
 
+  /// Method to send a message to the ChatGPT API and receive a response.
+  /// It also remembers the context of your messages. If you want to forget the
+  /// context use forget() method
+  ///
+  /// [message]: The message to send to the ChatGPT API.
+  /// Returns a future containing the response from the ChatGPT API.
   Future<String> answer({required String message}) async {
     try {
       final Map<String, dynamic> answer = (await dio.post("/gpt-3.5", data: {
@@ -45,8 +69,9 @@ class ChatGPT {
       } else {
         logPrint("Error happened with Chad GPT API!");
         throw ChatGPTException(
-            errorCode: answer["error_code"],
-            errorMessage: answer["error_message"]);
+          errorCode: answer["error_code"],
+          errorMessage: answer["error_message"],
+        );
       }
     } on DioException catch (e) {
       if (e.response != null) {
@@ -60,18 +85,8 @@ class ChatGPT {
       rethrow;
     }
   }
-}
 
-Future<void> exampleRun() async {
-  ChatGPT chatGPT =
-      ChatGPT(apiKey: const String.fromEnvironment("CHAD_GPT_API_KEY"));
-  String response = await chatGPT.answer(message: "Йо, как дела?");
-  print(response);
-  response = await chatGPT.answer(message: "А у меня вот плохо.");
-  print(response);
-}
-
-void main() {
-  // getHttp();
-  exampleRun();
+  void forget() {
+    history = [];
+  }
 }
