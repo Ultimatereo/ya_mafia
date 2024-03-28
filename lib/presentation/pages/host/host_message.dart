4ff94f15ffdeb63aef4a12fb49eddad991a2c448
Ultimatereo <mansursult9@gmail.com>
2024-03-28 23:37:08 +0300

    implemented support voic for different locales (still has some bugs)

--- lib/presentation/pages/host/host_message.dart
+++ lib/presentation/pages/host/host_message.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_tts/flutter_tts.dart';
+import 'package:ya_mafia/zgen/i18n/strings.g.dart';
 
 const String str =
     "Дамы и господа, сегодняшняя ночь в игре Мафия была наполнена интригой и неожиданными поворотами событий! На нашем сегодняшнем городском совещании выяснилось, что мафия попыталась совершить убийство - целью оказался наш уважаемый горожанин Антон. Однако, противостоять смерти смог благодаря действиям нашего доблестного доктора, который своевременно оказал ему помощь и предотвратил трагедию! Сейчас важно остаться бдительными и держать ухо востро - мафия продолжает свои попытки, но благодаря нашим гражданским усилиям мы сможем выявить их и предотвратить новые атаки. Все вместе мы можем победить в этой схватке за нашу благополучную городскую жизнь!";
@@ -12,11 +13,11 @@ class HostMessage extends StatefulWidget {
 }
 
 class _HostMessageState extends State<HostMessage> {
-  FlutterTts _flutterTts = FlutterTts();
+  final FlutterTts _flutterTts = FlutterTts();
   final TextEditingController voiceController = TextEditingController();
 
   Map<String, String>? _selectedVoice;
-  List<Map<String, String>> _voices = [];
+  final Map<String, List<Map<String, String>>> _voices = {};
 
   @override
   void initState() {
@@ -27,37 +28,37 @@ class _HostMessageState extends State<HostMessage> {
   void initTTS() {
     _flutterTts.getVoices.then((data) {
       try {
-        // TODO: fix this amazing costil'
-        // TODO: Подгружай два списка моделек для двух языков и отображай тот что надо в build
-        _voices = List<Map>.from(data)
+        final List<Map<String, String>> voices = List<Map>.from(data)
             .map((Map m) => m.map((key, value) =>
                 MapEntry<String, String>(key.toString(), value.toString())))
             .toList();
-        _voices = _voices
-            .where((voice) =>
-                voice["locale"]!.contains("ru") ||
-                voice["locale"]!.contains("en"))
-            .toList();
+        for (Map<String, String> voice in voices) {
+          String key =
+              voice["locale"]?.substring(0, voice["locale"]?.indexOf('-')) ??
+                  "en";
+          _voices.putIfAbsent(key, () => <Map<String, String>>[]);
+          _voices[key]!.add(voice);
+        }
+        setVoice(_voices[context.t.language]?.first);
+        _flutterTts.speak(str);
         print(_voices);
-        setState(() {
-          _selectedVoice = _voices.first;
-          setVoice(_selectedVoice);
-          _flutterTts.speak(str);
-        });
       } catch (e) {
         print(e);
       }
     });
   }
 
-  void setVoice(Map? voice) {
-    _flutterTts.setVoice({"name": voice!["name"], "locale": voice["locale"]});
+  void setVoice(Map<String, String>? voice) {
+    if (voice != null) {
+      _flutterTts
+          .setVoice({"name": voice["name"]!, "locale": voice["locale"]!});
+    }
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: _buildUI(),
+      body: _buildUI(context),
       floatingActionButton: Row(
         mainAxisAlignment: MainAxisAlignment.end,
         children: [
@@ -82,7 +83,7 @@ class _HostMessageState extends State<HostMessage> {
     );
   }
 
-  Widget _buildUI() {
+  Widget _buildUI(BuildContext context) {
     return SafeArea(
       child: Center(
         child: Column(
@@ -90,32 +91,34 @@ class _HostMessageState extends State<HostMessage> {
           mainAxisAlignment: MainAxisAlignment.center,
           // crossAxisAlignment: CrossAxisAlignment.center,
           children: [
-            _speakerSelector(),
+            _speakerSelector(context),
           ],
         ),
       ),
     );
   }
 
-  Widget _speakerSelector() {
+  Widget _speakerSelector(BuildContext context) {
     return DropdownMenu<Map<String, String>>(
       width: 300,
       initialSelection: _selectedVoice,
       controller: voiceController,
       requestFocusOnTap: true,
-      label: const Text('Voice'),
+      label: Text(context.t.voice),
       onSelected: (Map<String, String>? voice) {
         setState(() {
           _selectedVoice = voice;
           setVoice(voice);
         });
       },
-      dropdownMenuEntries: _voices.map((Map<String, String> voice) {
-        return DropdownMenuEntry<Map<String, String>>(
-          value: voice,
-          label: voice["name"]!,
-        );
-      }).toList(),
+      dropdownMenuEntries:
+          _voices[context.t.language]?.map((Map<String, String> voice) {
+                return DropdownMenuEntry<Map<String, String>>(
+                  value: voice,
+                  label: voice["name"]?? "unknown",
+                );
+              }).toList() ??
+              <DropdownMenuEntry<Map<String, String>>>[],
     );
   }
 }
